4d5732d81b4ea72ea54168f7fe96edd137229cb4
Daniel Kardach <danielkardach@gmail.com>
2015-07-24 11:23:35 +0200

    canCompactDatabase, canCompactFileSystem, resolves #30, resolves #31

--- src/test/groovy/net/ravendb/pages/ManageServerCompactPage.groovy
+++ src/test/groovy/net/ravendb/pages/ManageServerCompactPage.groovy
@@ -1,10 +1,14 @@
 package net.ravendb.pages
 
-import net.ravendb.modules.ManageServerMenu;
 import geb.Page
+import net.ravendb.modules.AlertTextModule
+import net.ravendb.modules.ManageServerMenu
 
 
 class ManageServerCompactPage extends Page {
+
+    final static DB_SUCCESS = "Database was successfully compacted!"
+    final static FS_SUCCESS = "File system was successfully compacted!"
 
     static at = {
         startCompactionButton
@@ -13,7 +17,31 @@ class ManageServerCompactPage extends Page {
 
     static content = {
         menu { module ManageServerMenu }
+        messagesContainer { module AlertTextModule }
+
+        databseTab { $("a[href='#compactDb']") }
+        fileSystemTab { $("a[href='#compactFs']") }
 
         startCompactionButton { $("button", text:"Start Compaction") }
+
+        databaseNameInput { $("input[name='databaseName']") }
+        fileSystemNameInput { $("input[name='filesystemName']") }
+        startCompactionButton { $("button", text:"Start Compaction") }
+    }
+
+    def compactDatabase(String name) {
+        databaseNameInput = name
+        databseTab.click()
+        startCompactionButton.click()
+
+        waitFor(10, 0.1) { messagesContainer.containsMessage(DB_SUCCESS) }
+    }
+
+    def compactFileSystem(String name) {
+        fileSystemNameInput = name
+        fileSystemTab.click()
+        startCompactionButton.click()
+
+        waitFor(10, 0.1) { messagesContainer.containsMessage(FS_SUCCESS) }
     }
 }
