71f283585d5cb1de4a89e53c328a19c3c331fa46
Daniel Kardach <danielkardach@gmail.com>
2015-08-21 11:08:35 +0200

    canViewTrafficWatch test, resolves #49

--- src/test/groovy/net/ravendb/pages/manage/ManageServerTrafficWatchPage.groovy
+++ src/test/groovy/net/ravendb/pages/manage/ManageServerTrafficWatchPage.groovy
@@ -6,14 +6,27 @@ import geb.Page
 
 class ManageServerTrafficWatchPage extends Page {
 
+    public final static String RESOURCE_SYSTEM_DATABASE = "<system>"
+
     static at = {
-        configureConnectionButton
-        menu
+        configureConnectionButton.displayed
+        menu.toSystemDatabaseLink.displayed
     }
 
     static content = {
         menu { module ManageServerMenu }
 
         configureConnectionButton { $("button", text:"Configure Connection") }
+        reconnectConnectionButton { $("button", text:"Reconnect") }
+        disconnectConnectionButton { $("button", text:"Disconnect") }
+
+        statusConnectedLabel { $("label", text:"Connected") }
+        statusDisconnectedLabel { $("label", text:"Disconnected") }
+        logsContainer { $("div#logRecordsContainer") }
+
+        // modal dialog
+        maxEntriesInput(required:false) { $("input#maxEntries") }
+        resourceNameInput(required:false) { $("input#watchedResource") }
+        connectButton(required:false) { $("button", text:"Connect") }
     }
 }
