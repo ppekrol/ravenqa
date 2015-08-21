936bfe4d9e56a1a66562c5394cae2fc7cfebb487
Daniel Kardach <danielkardach@gmail.com>
2015-08-21 12:39:29 +0200

    canRunDiskIOTest test, resolves #52

--- src/test/groovy/net/ravendb/pages/manage/ManageServerIoTestPage.groovy
+++ src/test/groovy/net/ravendb/pages/manage/ManageServerIoTestPage.groovy
@@ -6,14 +6,52 @@ import geb.Page
 
 class ManageServerIoTestPage extends Page {
 
+    public final static String TEST_TYPE_SIMPLE = "Simple"
+    public final static String TEST_TYPE_BATCH = "Batch"
+    public final static String OPERATION_TYPE_READ = "Read"
+    public final static String OPERATION_TYPE_WRITE = "Write"
+    public final static String OPERATION_TYPE_READ_AND_WRITE = "Read and write"
+    public final static String BUFFERING_READ = "Read"
+    public final static String BUFFERING_NONE = "None"
+    public final static String BUFFERING_READ_AND_WRITE = "Read and write"
+    public final static String SEQUENTIAL_YES = "Yes"
+    public final static String SEQUENTIAL_NO = "No"
+
     static at = {
-        testDiskPerformanceButton
-        menu
+        testDiskPerformanceButton.displayed
+        menu.toSystemDatabaseLink.displayed
     }
 
     static content = {
         menu { module ManageServerMenu }
 
         testDiskPerformanceButton { $("button", text:"Test Disk Performance") }
+
+        dirLocationInput { $("input#path") }
+        fileSizeInput { $("input#fileSize") }
+        testTypeButton { $("button#testType") }
+        operationTypeButton { $("button#operationType") }
+        bufferingButton { $("button#bufferingType") }
+        sequentialButton { $("button#sequential") }
+        threadCountInput { $("input#threadCount") }
+        timeToRunInput { $("input#timeToRunInSeconds") }
+        chunkSizeInput { $("input#chunkSize") }
+
+        testResultsHeader(required:false) { $("div", text:"Disk Performance Test Results") }
+    }
+
+    def chooseOption(def select, def option) {
+        select.click()
+
+        def itemToClick
+        select.parent().$("li").each {
+            if(it.text().equals(option)) {
+                itemToClick = it
+            }
+        }
+
+        if(itemToClick) {
+            itemToClick.click()
+        }
     }
 }
