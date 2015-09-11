1299165574fb84678fac40e742b518dc4d60027e
Agnieszka Bruska <agabruska@ais.pl>
2015-09-11 10:34:32 +0200

    resolves #46

--- src/test/groovy/net/ravendb/pages/DetailsIndexPage.groovy
+++ src/test/groovy/net/ravendb/pages/DetailsIndexPage.groovy
@@ -1,11 +1,14 @@
 package net.ravendb.pages
 
 import geb.Page
+import net.ravendb.modules.ChooseColumnsModalDialog
 import net.ravendb.modules.QueryStatsModalDialog
 import net.ravendb.modules.TopNavigationBar
 
 
 class DetailsIndexPage extends Page {
+
+	final static String INDEX_QUERY_RESULTS_COLUMN_COMPANY = "Company"
 
     static at = {
         queryStatsButton
@@ -14,11 +17,14 @@ class DetailsIndexPage extends Page {
     static content = {
 		topNavigation { module TopNavigationBar }
 		queryStatsModalDialog { module QueryStatsModalDialog }
+		chooseColumnsModalDialog { module ChooseColumnsModalDialog }
 
 		runQueryButton { $("button[title='Run the query (Alt+R)']") }
+		chooseColumnsButton { $("button[title='Choose columns...']") }
 		queryStatsButton { $("a[title='Show Query Stats In Dialog']") }
 
 		queryResultsList { $("div#queryResultsGrid div.ko-grid-row") }
+		queryResultsListHeaders(required:false) { $("div.ko-grid-column-header span[data-bind='text: header']") }
     }
 
 	int getRowsCount() {
@@ -30,6 +36,17 @@ class DetailsIndexPage extends Page {
 		}
 
 		return rowsCount
+	}
+
+	boolean isHeaderPresent(String title) {
+		boolean present = false
+		queryResultsListHeaders.each {
+			if(it.text().equals(title)) {
+				present = true
+			}
+		}
+
+		return present
 	}
 
 }
