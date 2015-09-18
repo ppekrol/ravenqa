0e82de69f04dca29f1fa46126a3a9b5d0f228f96
Agnieszka Bruska <agabruska@ais.pl>
2015-09-18 08:54:37 +0200

    resolves #58

--- src/test/groovy/net/ravendb/test/IndexesTest.groovy
+++ src/test/groovy/net/ravendb/test/IndexesTest.groovy
@@ -2,6 +2,7 @@ package net.ravendb.test
 
 import net.ravendb.modules.QueryStatsModalDialog
 import net.ravendb.pages.DetailsIndexPage
+import net.ravendb.pages.DocumentPage
 import net.ravendb.pages.DocumentsPage
 import net.ravendb.pages.EditIndexPage
 import net.ravendb.pages.IndexMergeSuggestionsPage
@@ -147,15 +148,42 @@ class IndexesTest extends DatabaseWithSampleDataTestBase {
     }
 
     /**
-     * User can go to index details page.
+     * User can go to index details page and view query stats.
      * @Step Navigate to Indexes page.
      * @Step Click on the index name.
-     * @Step User can run the query and review loaded index`s documents.
      * @Step User can view query stats.
-     * @verification Query results list and Query Stats modal dialog displayed.
+     * @verification Query Stats modal dialog displayed.
      */
     @Test(groups="Smoke")
     void canManageIndexDetails() {
+        at DocumentsPage
+
+        topNavigation.indexesLink.click()
+        waitFor { at IndexesPage }
+
+        getIndexLink(URLEncoder.encode(IndexesPage.INDEX_NAME_ORDERS_BY_COMPANY, "UTF-8")).click()
+        waitFor { at DetailsIndexPage }
+
+        // view query stats
+        queryStatsButton.click()
+        waitFor { queryStatsModalDialog.header.displayed }
+        waitFor { queryStatsModalDialog.getQueryStatsData(QueryStatsModalDialog.QUERY_STATS_TOTAL_RESULTS).displayed }
+        waitFor { queryStatsModalDialog.getQueryStatsData(QueryStatsModalDialog.QUERY_STATS_STATUS).displayed }
+        waitFor { queryStatsModalDialog.okButton.displayed }
+
+        queryStatsModalDialog.okButton.click()
+        waitFor { at DetailsIndexPage }
+    }
+
+    /**
+     * User can view document by clicking on it on query results list.
+     * @Step Navigate to Indexes page.
+     * @Step Click on the index name.
+     * @Step Click on the document link.
+     * @verification Document page displayed.
+     */
+    @Test(groups="Smoke")
+    void canViewDocumentByClickingOnItOnQueryResultsList() {
         at DocumentsPage
 
         topNavigation.indexesLink.click()
@@ -169,15 +197,11 @@ class IndexesTest extends DatabaseWithSampleDataTestBase {
         waitFor { queryResultsList.size() > 0 }
         waitFor { getRowsCount() > 0 }
 
-        // view query stats
-        queryStatsButton.click()
-        waitFor { queryStatsModalDialog.header.displayed }
-        waitFor { queryStatsModalDialog.getQueryStatsData(QueryStatsModalDialog.QUERY_STATS_TOTAL_RESULTS).displayed }
-        waitFor { queryStatsModalDialog.getQueryStatsData(QueryStatsModalDialog.QUERY_STATS_STATUS).displayed }
-        waitFor { queryStatsModalDialog.okButton.displayed }
+        // view document by clicking on it
+        clickDocument(DetailsIndexPage.INDEX_QUERY_RESULTS_COLLECTION_COMPANIES_DOCUMENT)
+        waitFor { at DocumentPage }
 
-        queryStatsModalDialog.okButton.click()
-        waitFor { at DetailsIndexPage }
+        assert documentNameInput.value() == DetailsIndexPage.INDEX_QUERY_RESULTS_COLLECTION_COMPANIES_DOCUMENT
     }
 
     /**
