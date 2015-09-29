30f53b68645ca959feb1894945516c3f12cf5bbc
Agnieszka Bruska <agabruska@ais.pl>
2015-09-29 13:13:46 +0200

    resolves #96

--- src/test/groovy/net/ravendb/pages/TransformersPage.groovy
+++ src/test/groovy/net/ravendb/pages/TransformersPage.groovy
@@ -1,6 +1,7 @@
 package net.ravendb.pages
 
 import geb.Page
+import net.ravendb.modules.AlertTextModule
 import net.ravendb.modules.TopNavigationBar
 
 
@@ -13,11 +14,18 @@ class TransformersPage extends Page {
     static content = {
 		// modules
         topNavigation { module TopNavigationBar }
+        messagesContainer { module AlertTextModule }
 
         // tool bar
         newTransformerButton { $("a[title='Add a new transformer (Alt+N)']") }
+        pasteTransformerButton { $("button[title='Paste transformer from JSON (Alt+V)']") }
 
         transformerLinks(required:false) { $("a[href^='#databases/transformers/edit/']") }
+
+        // paste transformer from JSON modal dialog
+        pasteTransformerFromJsonModalHeader { $("h4", text:"Paste Transformer") }
+        transformerAceEditorPasteFromJson { $("pre.ace_editor.ace-xcode.ui-resizable textarea") }
+        saveTransformerButton { $("button[title='Save transformer (Alt+S)']") }
     }
 
     def getTransformerLink(CharSequence name) {
@@ -28,5 +36,16 @@ class TransformersPage extends Page {
             }
         }
         link
+    }
+
+    def createAndSaveTransformerFromJson(String name, String json) {
+        pasteTransformerButton.click()
+        waitFor { pasteTransformerFromJsonModalHeader.displayed }
+        transformerAceEditorPasteFromJson = json
+
+        waitFor { !(saveTransformerButton.@disabled == 'true') }
+        saveTransformerButton.click()
+
+        messagesContainer.waitForMessage("Saved " + name)
     }
 }
