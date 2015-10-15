edddfacc240a2286291307623f038a2a99899fa9
Agnieszka Bruska <agabruska@ais.pl>
2015-10-15 14:57:34 +0200

    canAddDatabaseCustomFunctions, resolves #105

--- src/test/groovy/net/ravendb/pages/SettingsPage.groovy
+++ src/test/groovy/net/ravendb/pages/SettingsPage.groovy
@@ -2,6 +2,7 @@ package net.ravendb.pages
 
 import geb.Page
 import net.ravendb.modules.TopNavigationBar
+import net.ravendb.modules.manage.ManageServerCustomFunctions
 import net.ravendb.modules.manage.ManageServerReplication
 
 
@@ -14,11 +15,13 @@ class SettingsPage extends Page {
     static content = {
         // modules
         topNavigation(required:false) { module TopNavigationBar }
+        customFunctions { module ManageServerCustomFunctions }
         manageServerReplication { module ManageServerReplication }
 
         // content
         settingsBreadcrumb { $("ul.breadcrumb span", text:"Settings") }
 
+        databaseCustomFunctionsLink(required:false) { $("a[href^='#databases/settings/customFunctionsEditor']") }
         databaseSQLReplicationLink(required:false) { $("a[href^='#databases/settings/sqlReplication?']") }
         databaseReplicationLink(required:false) { $("a[href^='#databases/settings/replication']") }
         databaseVersioningLink(required:false) { $("a[href^='#databases/settings']") }
